541523ce9e48d2a2a44f7a25075c3278c195a0c4
Mark Withall <github@markwithall.com>
2016-01-20 15:35:29 +0000

    Move TESTickle files to own folder.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-include TESTickle.mk
+include TESTickle/TESTickle.mk
 
 ifeq ($(OS),Windows_NT)
 	RM := cmd /C del
@@ -11,7 +11,7 @@ endif
 .PHONY: test clean
 
 sample.test$(DOTEXE): $(call test_files_for,sample.test.c)
-	$(CC) $(CFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) -ITESTickle -o $@ $^
 
 test: sample.test$(DOTEXE)
 	prove -f -e "" $^
--- TESTickle/TESTickle.mk
+++ TESTickle/TESTickle.mk
@@ -4,11 +4,11 @@ PERL=perl
 
 TESTICKLE_BUILT_ITEMS=*.test.runner.c *.test.o *.test *.test.exe
 
-%.test.o: %.test.c TESTickle.h
-	$(CC) $(CFLAGS) -o $@ -c $<
+%.test.o: %.test.c TESTickle/TESTickle.h
+	$(CC) $(CFLAGS) -ITESTickle -o $@ -c $<
 
 %.test.runner.c: %.test.c
-	$(PERL) test_generator.pl $< > $@
+	$(PERL) TESTickle/test_generator.pl $< > $@
 
 test_files_for = $(patsubst %.test.c,%.test.runner.c,$(1)) $(patsubst %.test.c,%.test.o,$(1))
 
